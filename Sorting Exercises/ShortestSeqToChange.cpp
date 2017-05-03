53bd7e97177bd3299f3065baa96378dde514c5ef
aheadwhat <chengh37@mail2.sysu.edu.cn>
2017-05-03 19:53:08 +0800

    Create ShortestSeqToChange.cpp
    对于一个数组，请设计一个高效算法计算需要排序的最短子数组的长度。
    给定一个int数组A和数组的大小n，请返回一个二元组，代表所求序列的长度。(原序列位置从0开始标号,若原序列有序，返回0)。保证A中元素均为正整数

---
+++ Sorting Exercises/ShortestSeqToChange.cpp
@@ -0,0 +1,26 @@
+class Subsequence {
+public:
+    int shortestSubsequence(vector<int> A, int n) {
+        // write code here
+        if(n<=1) return 0;
+        int max=A[0],right=-1;
+        int min=A[n-1],left=-1;
+        for(int i=0;i<n;i++){
+            if(A[i]>max){
+                max=A[i];
+            }
+            else{
+                right=i;
+            }
+        }
+        for(int i=n-1;i>=0;i--){
+            if(A[i]>min){
+                left=i;
+            }
+            else{
+                min=A[i];
+            }
+        }
+        return (right==-1||left==-1)?0:right-left+1;
+    }
+};
