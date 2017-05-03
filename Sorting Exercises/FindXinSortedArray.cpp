0326a706a52a2242d75c38de29fbea4989db0538
aheadwhat <chengh37@mail2.sysu.edu.cn>
2017-05-03 19:19:24 +0800

    Create FindXinSortedArray.cpp
    现在有一个行和列都排好序的矩阵，请设计一个高效算法，快速查找矩阵中是否含有值x。
    给定一个int矩阵mat，同时给定矩阵大小nxm及待查找的数x，请返回一个bool值，代表矩阵中是否存在x。所有矩阵中数字及x均为int范围内整数。保证n和m均小于等于1000。

---
+++ Sorting Exercises/FindXinSortedArray.cpp
@@ -0,0 +1,19 @@
+# find the target number in a sorted array
+
+class Finder {
+public:
+    bool findX(vector<vector<int> > mat, int n, int m, int x) {
+        // write code here
+        if(n<1||m<1) return false;
+        int p=0;
+        int q=m-1;
+        while(p<n&&q>=0){
+            if(mat[p][q]==x) return true;
+            else if(mat[p][q]>x){
+                --q;
+            }
+            else ++p;
+        }
+        return false;
+    }
+};
